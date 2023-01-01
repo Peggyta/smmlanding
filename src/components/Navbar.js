f747e4f7f90bdfa9dce94f8ff7bb2b56b623bfb9
Peggyta <peggyta9096@gmail.com>
2023-01-02 00:59:30 +0330

    navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,33 +3,55 @@ import '../styles/Navbar.css';
 //Image & Icons
 import logo from '../images/Logo.png';
 import divider from '../images/Divider.png';
-import glow1 from '../images/Glow-1.svg';
-
+import user from '../images/user.png';
+import hamburMenu from '../images/hambur.png';
 
 
 const Navbar = () => {
     return (
-        <div className='svg-container w-100 position-relative'>
-            <div id="left" className='svg-left position-absolute'> 
-            </div>
-            <div id="right" className='svg-right position-absolute'>
+        <>
+        <div className='svg-container'>
+            <div className='glow-images position-relative'>
+                <div className='svg-left position-absolute'> </div>
+                <div className='svg-right position-absolute'></div>
             </div>
                 <div className='nav-container'>
-                    <ul className='nav-list d-flex justify-content-center align-items-center list-unstyled'>
+                    <ul className='nav-list d-flex justify-content-center align-items-center list-unstyled position-relative z-index-1  '>
                         <li><a className='nav nav-link-one text-decoration-none' href='#'>Sign in</a></li>
                         <li><a className='nav nav-link-two text-decoration-none' href='#'>Sign up</a></li>
-                        <div>
+                        <div className='logo-container position-relative'>
                             <a href='#'><img src={logo} alt="logo" /></a>
+                            <div className='logo-glow'></div>
                         </div>
                         <li><a className='nav nav-link-three text-decoration-none' href='#'>API</a></li>
                         <li><a className='nav nav-link-four text-decoration-none' href='#'>Services</a></li>
                     </ul>
                 </div>
-                <div className='d-flex justify-content-center'>
-                    <img src={divider} alt='divider' />
-                </div>
-            
+            <div className='divider-container justify-content-center'>
+                <img src={divider} alt='divider' />
+            </div> 
         </div>
+        <div className='mobile-menu position-relative'>
+            <div className='mobile-svg position-absolute'></div>
+            <div className='mobile-items position-relative z-index-1'>
+                <div>
+                    <a href='#'>
+                        <img className='mobile-icons' src={user} alt='user' />
+                    </a>  
+                </div>
+                <div>
+                    <a href='#'>
+                        <img src={logo} alt='logo' />
+                    </a>    
+                </div>
+                <div>
+                    <a href='#'>
+                        <img className='mobile-icons' src={hamburMenu} alt='hambur-menu' />
+                    </a>
+                </div>
+            </div>   
+        </div>
+        </>
     );
 };
 
